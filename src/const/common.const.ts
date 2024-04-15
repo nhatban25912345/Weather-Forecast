4643ec5dc7260e130c245081e271e326e6d90a16
nhat12a2 <hoangnhat529@gmail.com>
2024-04-16 05:40:50 +0700

    fig bug https

--- src/const/common.const.ts
+++ src/const/common.const.ts
@@ -1,2 +1,2 @@
-export const WEATHER_BASE_URL = "http://api.openweathermap.org";
+export const WEATHER_BASE_URL = "https://api.openweathermap.org";
 export const WEATHER_API_KEY = "1c5da32bd6a0d1c4c017b21b49833c7f";
